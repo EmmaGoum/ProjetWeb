957b8b49f63a20fe159107cb5d7c23e2b8d34157
Unknown <egoumillout@ensc.fr>
2018-02-24 12:46:30 +0100

    [Se connecter]Création de la page

---
+++ includes/connexion.php
@@ -0,0 +1,32 @@
+<html>
+  <head>
+    <?php require "head.php"; ?>
+    <title> Responsum - Se connecter </title>
+  </head>
+
+  <body>
+    <?php require "header.php"; ?>
+
+    <h1> Se connecter </h1>
+
+    <form class=".form-horizontal" method="post" action="connect.php">
+        <div class="form-group">
+        <label class="control-label col-sm-2" for="email">Adresse mail : </label>
+          <div class="col-sm-10">
+            <input type="email" class="form-control" id="email" name="email"/>
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="mdp">Mot de passe : </label>
+          <div class="col-sm-10">
+            <input type="password" class="form-control" id="mdp" name="mdp"/>
+          </div>
+        </div>
+        <input type="submit" class="btn btn-default" value="Se connecter"/>
+    </form>
+
+    <?php require "footer.php"; ?>
+
+  </body>
+
+</html>
