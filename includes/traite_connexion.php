6e7c5096ef32c1b322d2203c210476db928dc6d5
Unknown <egoumillout@ensc.fr>
2018-02-27 10:57:53 +0100

    [connexion/déconnexion] modifications + création fichier traite_déconnexion

--- includes/traite_connexion.php
+++ includes/traite_connexion.php
@@ -1,22 +1,30 @@
 <?php
  require("connect.php");
  require("escape.php");
+ 
 
  if (empty(escape($_POST['email'])) || empty(escape($_POST['mdp']))) {
-   echo "test2";
+   echo "TEST";
 
  } else {
    $email = escape($_POST['email']);
    $mdp = escape($_POST['mdp']);
 
-   if (true ){
-       $reqCompte = $BDD -> prepare("SELECT idCompte FROM COMPTE WHERE mdp=$mdp AND mail=$email");
-       $reqCompte -> execute(array($email));
-       $reqCompte -> fetch();
+   $reqCompte = $BDD -> prepare("SELECT mdp, idCompte FROM COMPTE WHERE mail=?");
+   $reqCompte -> execute(array($email));
+   $variable = $reqCompte -> fetch();
+   $mdpUser = $variable['mdp'];
+   $idUser = $variable['idCompte'];
 
-       // Il faut mettre un lien vers la page d'accueil ici du coup ? Je comprends pas trop comment on fait pour signaler que c'est connecté
+   if ( $mdp == $mdpUser ) {
+    session_start();
+
+    $_SESSION['idCompte'] = $idUser;
+    header("location:../index.php");
+
    } else {
-       echo "ça va pas";
+       echo "Utilisateur non reconnu";
    }
- }
+}
+
  ?>
